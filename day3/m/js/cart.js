27151c2c5e05be2a8f4569be82ee6a5a6a59f4ec
yxDeveloper <dilutian0415@gmail.com>
2020-08-10 23:35:32 +0800

    cart.js mui.scroll

--- day3/m/js/cart.js
+++ day3/m/js/cart.js
@@ -4,5 +4,19 @@ $(() => {
 		indicators:false,
 		// 显示滚动条
 	});
-	
+	// 初始化上下拉
+	mui.init({
+		pullRefresh: {
+			container: "#refreshContainer",
+			down: {
+				auto: true,
+				callback: function () {
+					// 1.初始化页面	自动下拉刷新
+					let that = this;
+					// 定义一个全局的 下拉组件对象	使用里面的方法
+					
+				}
+			}
+		}
+	})
 });
