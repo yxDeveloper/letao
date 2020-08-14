135b4c997f70730920c116ec75969dc6a5a118af
yxDeveloper <dilutian0415@gmail.com>
2020-08-14 18:44:04 +0800

    common

--- day3/m/js/common.js
+++ day3/m/js/common.js
@@ -35,3 +35,30 @@ CT.getItemById = function (arr,id) {
 	});
 	return obj;
 }
+// 需要登录的ajax请求
+CT.loginUrl = '/m/user/login.html';
+CT.cartUrl = '/m/user/cart.html';
+CT.userUrl = 'm/user/index.html';
+CT.loginAjax = function (params) {
+	// params====》{}
+	$.ajax({
+		type: params.type || 'get',
+		url: params.url || '#',
+		data: params.data || '',
+		dataType: params.dataType || 'json',
+		success: function (data) {
+			/*未登录的处理 {error: 400, message: "未登录！"}
+			所有的需要登录的接口 没有登录返回这个数据*/
+			if(data.error == 400){
+			/*跳到登录页  把当前地址传递给登录页面  当登录成功按照这个地址跳回来*/
+			location.href = CT.loginUrl + '?returnUrl=' + location.href;
+			return false;
+			}else {
+				params.success && params.success(data);
+			}
+		},
+		error:function () {
+			mui.toast('服务器繁忙');
+		}
+	});
+};
